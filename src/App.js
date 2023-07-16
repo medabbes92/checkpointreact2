da649ccc96814c6e2ed1b18edda0ded2014c4d2d
med abbes <muhamed.abes@gmail.com>
2023-07-16 11:09:24 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import './component/Chec.css';
+import me from "./me.jpg"
 import Description from './component/Description';
 import Price from './component/Price';
 import Name from './component/Name';
@@ -9,26 +10,20 @@ import Image from './component/Image';
 import { Card, ListGroup } from 'react-bootstrap';
 
 
-
+const firstName = "Abbes";
 function App() {
+  
   return (
     <>
     
     <div className="App">
-    
-
-   
-      
-      
-      
-    
-
+  <div className='carte'>
       <Card style={{ width: '18rem' }}>
-      <Image variant="top" src="holder.js/100px180?text=Image cap" />
+      <Image className="pompe" variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
         <Card.Title><Name/></Card.Title>
         <Card.Text>
-        <Description/>
+        <Description/> 
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
@@ -37,6 +32,11 @@ function App() {
       </ListGroup>
      
     </Card>
+</div>
+
+    <p>Hello, {firstName ? firstName : "there"}!</p>
+
+{firstName && <img className='mee' src={me} alt="Profile" />}
 
 
     </div>
--- src/component/Producte.js
+++ src/component/Producte.js
@@ -2,8 +2,9 @@ import aa from "./aa.jpg"
 const Producte = {
     Name: "Electropompe electrique",
     Price: "Prix : 3000 dt",
-    Description: "Moteur pour distribuer l'eau",
+    Description: "Electropompe immergée semi-axiale  corps en fonte 6 débit max.700 l/min 3 turbines en Noryle - moteur immergé 4 pouces - puissance nominale 5,5CV - monophasé",
     Image : <img src={aa} alt ='myImage' />
+    
   };
   
   export default Producte;
